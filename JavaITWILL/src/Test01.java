cc101402d0c47e845d7a8fcf391db2d30b4d79e1
Roh-JH <48000423+Roh-JH@users.noreply.github.com>
2019-02-26 12:35:27 +0900

    Update Test01.java

--- JavaITWILL/src/Test01.java
+++ JavaITWILL/src/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요");
+		System.out.println("GitHub에서 작성한 문장입니다");
 	}
 
 }
